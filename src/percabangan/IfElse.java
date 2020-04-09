c9f2891fa0560d942abcfe1b36c8cf373158d5a6
alvireza <62271791+alvireza@users.noreply.github.com>
2020-04-09 16:55:57 +0800

    jika kondisi1 salah, kondisi2 benar, statment jalan

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,14 +2,16 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 25000;
-        double uangDidompet = 53000;
+        double totalBelanja = 53000;
+        double uangDidompet = 25000;
 
         if(uangDidompet<totalBelanja) {
-
             System.out.println("Uang Kurang, Kurangi Jajan Anda");
+        }else if(uangDidompet>totalBelanja) {
+            double angsul = uangDidompet - totalBelanja;
+            System.out.println("Uang Cukup, Angsul : " + angsul);
         }else{
-            System.out.println("Uang Cukup, Beralaan Lahh");
+            System.out.println("Uang pas.. Uwu..");
         }
     }
 }
