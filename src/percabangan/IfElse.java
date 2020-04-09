9614e08ec0b28f401ff9fe0461d3088b2f88557c
alvireza <62271791+alvireza@users.noreply.github.com>
2020-04-09 16:50:37 +0800

    jika kondisi salah statment 2

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,14 +2,14 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 53000;
-        double uangDidompet = 25000;
+        double totalBelanja = 25000;
+        double uangDidompet = 53000;
 
         if(uangDidompet<totalBelanja) {
 
             System.out.println("Uang Kurang, Kurangi Jajan Anda");
         }else{
-            System.out.println("Uang Cukup, Selamat Menikmati");
+            System.out.println("Uang Cukup, Beralaan Lahh");
         }
     }
 }
